990d6235c68510b5fae54effc41d93b1f9175bbc
Larry Brewer <lbrewer@usa.m3.com>
2012-08-21 15:46:34 -0400

    initial project commit

---
+++ gen/com/lawrencebrewer/soundboard/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lawrencebrewer.soundboard;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/lawrencebrewer/Splashscreen.java
@@ -0,0 +1,14 @@
+package com.lawrencebrewer;
+
+import com.lawrencebrewer.soundboard.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splashscreen extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
